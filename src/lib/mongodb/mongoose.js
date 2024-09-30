8f93ea8f3ff07558751e77d455cc4423614d6474
pammu53 <pramodsavant2001@gmail.com>
2024-09-30 13:47:54 +0530

    mongo uri bug

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -6,7 +6,7 @@ export const connectDB = async () => {
     if (initialized) return; // Avoid reconnecting if already connected
 
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             dbName: 'myDatabase', // Specify the database name
